b1836d796f7ee4d6e9238c6d8835d6440f048d78
Nick Zarkadas <nzarko17@gmail.com>
2016-03-28 19:48:56 +0300

    Lisa's Workbook Passes all tests.

--- algorithms/implementation/lisa_workbook.cpp
+++ algorithms/implementation/lisa_workbook.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int n,k;
+   cin >> n >> k;
+   int page= 0, pr_no=0, res=0;
+   for (int chp=1; chp<=n; chp++) {
+      page++;
+      cin >> pr_no;
+      for(int problem=1; problem <= pr_no; problem++) {
+         if (problem == page)
+            res+=1;
+         if(problem % k==0 && problem < pr_no)
+            page++;
+      }
+   }
+                                                   
+   cout << res << endl;
     return 0;
 }
